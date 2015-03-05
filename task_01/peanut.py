b7e11604783c98179edaeee14fba322b64c2f995
Logan Thomson <logan.t.music@gmail.com>
2015-03-05 02:12:05 +0000

    Here are the modules and package for week 6 warmups

---
+++ task_01/peanut.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""A Basic Module"""
+
+BUTTER = 'I am truthy'
+
+OIL = ''
---
+++ task_03.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""A Module copying task_01"""
+
+BUTTER = 'I am truthy'
+
+JELLY = BUTTER
